ac6d048abf920b73cfce718b124a2a923de0c61f
Amina <minassouane@gmail.com>
2026-03-10 15:12:55 +0100

    feat(about): add About me content and styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,14 @@
+import styles from "./About.module.css";
+
 export function About() {
   return (
-    <>
+    <main className={styles.container}>
       <h1>
         About <span className="violet">me</span>
       </h1>
       <p>
-        Hi, I'm Amina Assouane, a passionate full-stack developer from Algeria.
+        Hi, I'm Amina Assouane, a passionate Full Stack Developer from Algeria.
       </p>
-    </>
+    </main>
   );
 }
