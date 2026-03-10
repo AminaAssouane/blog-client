2ae7602661d770dbcd3a5e517de3a9fa64303cc2
Amina <minassouane@gmail.com>
2026-03-10 08:16:32 +0100

    style(header): add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router";
+import logo from "../../assets/icons/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>
-        Amina's <span className="violet">blog</span>
+      <h1 className={styles.navLeft}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <div>
+          Amina's <span className="violet">blog</span>
+        </div>
       </h1>
-      <nav className={styles.navBar}>
+      <nav className={styles.navRight}>
         <Link to="/">
           <a>Home</a>
         </Link>
